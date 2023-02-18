5d02f06030af8b6c2d59d2ae991eaf399ee71810
Michael Strother <sportflier@live.com>
2023-02-18 15:14:27 -0500

    Clean the Nvidia cache.

---
+++ scripts/clean-cache.ps1
@@ -0,0 +1,40 @@
+function Remove-NvidiaSubcache($subfolder){
+    $nvidia_cache = $env:USERPROFILE + "/AppData/Local/NVIDIA" + $subfolder
+    Remove-AllFiles -folder $nvidia_cache
+}
+
+function Remove-AllFiles($folder){
+    $files = Get-ChildItem -Path $folder -File
+
+    foreach ($fileItem in $files) {
+        try {
+            $ErrorActionPreference = "Stop"
+            Remove-Item $fileItem.FullName
+            Write-Host ("Removed " + $fileItem.FullName) -ForegroundColor Cyan
+        }
+        catch {
+            <#Do this if an exception happens#>
+            Write-Host ("Error removing " + $fileItem.FullName) -ForegroundColor Yellow -BackgroundColor Magenta
+        }
+        finally {
+            $ErrorActionPreference = "Continue"
+        }
+    }
+
+    $subfolders = Get-ChildItem -Path $folder -Directory
+
+    foreach ($subfolder in $subfolders) {
+        Remove-AllFiles -folder $subfolder.FullName
+    }
+
+}
+
+function Remove-NvidiaCache {
+    Remove-NvidiaSubcache -subfolder "/DXCache"
+    Remove-NvidiaSubcache -subfolder "/GLCache"
+}
+
+Remove-NvidiaCache
+
+Write-Host "Use Disk Cleanup to clear the DirectX Shader Cache." -ForegroundColor Green
+Start-Process -FilePath CleanMgr.exe 
